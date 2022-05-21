f9e54a607bba51308c350f7a8e8336672470da67
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-20 21:39:34 -0300

    feat: add content fetched posts
    This commit render content of fetched posts on homepage.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Hello There!</h1>
+  <div>
+    <h1 class="text-7xl font-black text-center">New on the blog</h1>
+    <div v-for="post in posts" :key="post.title">
+      <h2 class="mt-2 text-3xl font-semibold">{{post.title}}</h2>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
