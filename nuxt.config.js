0f89b0865bf443b3f177bc7c7b109f487c04cc05
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-20 22:10:38 -0300

    chore: add presets images

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,4 +42,27 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  image: {
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536,
+    },
+    presets: {
+      preview: {
+        modifiers: {
+          fit: 'cover',
+          format: 'jpg',
+          width: 400,
+          height: 300,
+        },
+      },
+    },
+    }
+
 }
