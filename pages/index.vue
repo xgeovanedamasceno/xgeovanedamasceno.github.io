88b32f3fae93bf2f3eb05da1357f02e66ba80f86
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-20 21:33:22 -0300

    feat: add asyncData nuxt function
    This commit add asyncData built-in Nuxt function to fetch the post data
    to make them available to homepage.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial />
+  <h1>Hello There!</h1>
 </template>
 
 <script lang="ts">
@@ -7,5 +7,17 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  async asyncData( { $content } ): Promise<{ posts: any }> {
+    const posts = await $content()
+      .only(['title', 'image',  'tags', 'slug'])
+      .sortBy('createdAt', 'desc')
+      .fetch()
+
+      console.log("posts", posts)
+
+      return {
+        posts,
+      }
+  },
 })
 </script>
